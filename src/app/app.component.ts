5337075c99956592a076792f4ab14adf05f86f30
Cauê Reple <replecaue@gmail.com>
2022-03-14 15:45:46 +0100

    Introducing Angular concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prova';
+  title = 'applicazione di prova';
+  nome = 'Cauê';
+  punti = '200';
 }
