3b023ef3434312e02fa703ce2d796eaf1a1d1cf0
Cauê Reple <replecaue@gmail.com>
2022-03-15 11:30:23 +0100

    adding login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = "username";
+  password = "password";
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  autenticazione = () => console.log("Username: " + this.username + "\n" + "Password: " + this.password)
 
 }
