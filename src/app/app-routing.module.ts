e674d6497965b4ddd2df7a1b3246a45cb779b648
Cauê Reple <caue-reple@hotmail.com>
2022-03-16 10:07:40 +0100

    creating the error page and configuring the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' , component :LoginComponent},
+  { path:'index' , component :LoginComponent},
+  { path:'login' , component :LoginComponent}, //identifica la landing page
+  { path:'welcome' , component :WelcomeComponent},
+  { path:'**' , component :ErrorComponent}, //la pagina di errore viene identificata con doppio asterisco
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
